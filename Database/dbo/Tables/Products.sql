e0b7f9570bd1e39c88b839a5173be2269f50635b
Steven Follis <steven.follis@docker.com>
2017-10-19 16:50:35 +0000

    reset

--- Database/dbo/Tables/Products.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Products] (
-    [Id]          INT             IDENTITY (8, 1) NOT NULL,
-    [Name]        NVARCHAR (50)   NOT NULL,
-    [Description] NVARCHAR (1000) NOT NULL,
-    [Price]       MONEY           NOT NULL,
-    [ImageName]   NVARCHAR (50)   NULL,
-    CONSTRAINT [Products_PK__Products__0000000000000018] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
